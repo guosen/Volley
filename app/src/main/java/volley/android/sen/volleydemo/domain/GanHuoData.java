ea5459173f469f27adae9b4ee159ba8a1e3d49e7
shouwang <shouwang@meiyaapp.com>
2016-04-14 15:34:38 +0800

    1干货APi2引入recycleView

---
+++ app/src/main/java/volley/android/sen/volleydemo/domain/GanHuoData.java
@@ -0,0 +1,27 @@
+package volley.android.sen.volleydemo.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by shouwang on 16/4/14.
+ */
+public class GanHuoData implements Serializable{
+//    "_id": "570d031c6776590f608f0c00",
+//            "createdAt": "2016-04-12T22:15:56.723Z",
+//            "desc": "\u77e5\u4e4e\u5ba2\u6237\u7aef",
+//            "publishedAt": "2016-04-13T12:20:44.64Z",
+//            "source": "chrome",
+//            "type": "Android",
+//            "url": "https://github.com/easonho/ZhihuPaper",
+//            "used": true,
+//            "who": "eason_ho"
+    public String  _id;
+    public String  createdAt;
+    public String  desc;
+    public String  publishedAt;
+    public String  source;
+    public String  type;
+    public String  url;
+    public boolean used;
+    public String  who;
+}
